4069514d81b57db144a52df591a7224513f10d92
Jim Cote <jfcote87@gmail.com>
2024-03-09 15:59:35 -0500

    sock_test.go fixed build args

--- sock_test.go
+++ sock_test.go
@@ -3,8 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 //
-//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || illumos
-// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris illumos
+// go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || illumos
 
 package sshdb_test
 
